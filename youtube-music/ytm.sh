48f1aae0351cddf99dec11e9f4d243bd9904c597
DTJW92 <96082560+DTJW92@users.noreply.github.com>
2024-11-08 23:59:18 +0000

    Update ytm.sh

--- youtube-music/ytm.sh
+++ youtube-music/ytm.sh
@@ -249,7 +249,7 @@ echo -e "${G}DOWNLOADING${W} $APPNAME . . ."
 sleep 1
 echo -e "${T}$APPLINK" | sed 's,https://,> ,g' | sed 's,http://,> ,g' 2>/dev/null
 cd $temp
-curl --progress-bar -L -o "$temp/youtube-music.sh" "$APPLINK"
+curl --progress-bar -L -o "$temp/youtube-music.AppImage" "$APPLINK"
 cd ~/
 mv $temp/* $APPPATH 2>/dev/null
 chmod a+x $APPPATH 2>/dev/null
