00049b3c57ecfcc0e76fb6786055e7a25b0d6629
DTJW92 <96082560+DTJW92@users.noreply.github.com>
2024-11-10 22:48:47 +0000

    Update psxdownloader.sh

--- PSXDownloader/psxdownloader.sh
+++ PSXDownloader/psxdownloader.sh
@@ -42,7 +42,7 @@ url_decode() {
 
 # Fetch list of game files from the URL and create a checklist
 url="https://myrient.erista.me/files/Internet%20Archive/chadmaster/chd_psx_eur/CHD-PSX-EUR/"
-game_list=($(curl -s $url | grep -oP 'href="\K[^"]*' | grep -E "\.chd$"))
+game_list=($(curl -s $url | sed 's/&amp;/\&/g; s/&lt;/</g; s/&gt;/>/g; s/&quot;/"/g; s/&apos;/\'\''/g' | grep -oP 'href="\K[^"]*' | grep -E "\.chd$"))
 
 
 if [ ${#game_list[@]} -eq 0 ]; then
