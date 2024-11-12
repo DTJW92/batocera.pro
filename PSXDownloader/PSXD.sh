34bc74b11434a5d916134de8ed0865eada3296cd
DTJW92 <d.weatherall92@gmail.com>
2024-11-12 02:59:58 +0000

    Update PSXD.sh

--- PSXDownloader/PSXD.sh
+++ PSXDownloader/PSXD.sh
@@ -1,2 +1,1 @@
-#!/bin/bash
-DISPLAY=:0.0 xterm -fs 30 -maximized -fg white -bg black -fa "DejaVuSansMono" -en UTF-8 -e bash -c "DISPLAY=:0.0  curl -L raw.githubusercontent.com/DTJW92/batocera.pro/main/PSXDownloader/psxd.sh | bash"
+DISPLAY=:0.0 xterm -fs 30 -maximized -fg white -bg black -fa "DejaVuSansMono" -en UTF-8 -e bash -c "DISPLAY=:0.0  curl -Ls raw.githubusercontent.com/DTJW92/batocera.pro/main/PSXDownloader/psxdownloader.sh | bash"
